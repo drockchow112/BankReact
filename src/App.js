2577c4b04d9235e991eea27a1bff830d106bc8a1
Derrick <derrick.chow234@gmail.com>
2020-06-08 18:01:39 -0400

    Updated State

--- src/App.js
+++ src/App.js
@@ -7,18 +7,40 @@ import LogIn from "./components/LogIn";
 import Debits from "./components/Debits";
 import AccountBalance from "./components/AccountBalance";
 import Credits from "./components/Credits";
+import axios from "axios";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      accountBalance: 14568.27,
+      debits: " ",
+      credits: " ",
       currentUser: {
         userName: "bob_loblaw",
         memberSince: "08/23/99"
       }
     };
+  }
+
+  componentDidMount() {
+    axios.get("https://moj-api.herokuapp.com/credits").then(res => {
+      const credits = res.data;
+      let creditAmount = 0;
+      for (let i = 0; i < credits.length; i++) {
+        creditAmount += credits[i].amount;
+      }
+      this.setState({ credits: creditAmount });
+    });
+
+    axios.get("https://moj-api.herokuapp.com/debits").then(res => {
+      const debits = res.data;
+      let debitAmount = 0;
+      for (let i = 0; i < debits.length; i++) {
+        debitAmount += debits[i].amount;
+      }
+      this.setState({ debits: debitAmount });
+    });
   }
 
   mockLogIn = logInInfo => {
@@ -38,20 +60,24 @@ class App extends Component {
 
     const debitComponent = () => (
       <>
-        <AccountBalance accountBalance={this.state.accountBalance} />
-        <Debits debit={2} />
+        <AccountBalance
+          accountBalance={this.state.debits - this.state.credits}
+        />
+        <Debits accountBalance={this.state.debits - this.state.credits} />
       </>
     );
 
     const creditComponent = () => (
       <>
-        <AccountBalance accountBalance={this.state.accountBalance} />
+        <AccountBalance
+          accountBalance={this.state.debits - this.state.credits}
+        />
         <Credits />
       </>
     );
 
     const HomeComponent = () => (
-      <Home accountBalance={this.state.accountBalance} />
+      <Home accountBalance={this.state.debits - this.state.credits} />
     );
     const UserProfileComponent = () => (
       <UserProfile
