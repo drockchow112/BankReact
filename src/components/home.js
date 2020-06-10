5a80f641019fc2a97952e955728853370bed7b19
Derrick <derrick.chow234@gmail.com>
2020-06-09 22:42:48 -0400

    Added styling, login functional

--- src/components/home.js
+++ src/components/home.js
@@ -1,20 +1,20 @@
 import React, { Component } from "react";
 import AccountBalance from "./AccountBalance";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import "./cssFiles/homepage.css";
+
 class Home extends Component {
   render() {
     return (
-      <div>
-        <img
-          src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png"
-          alt="bank"
-        />
+      <div className="homepage">
         <h1>Bank of React</h1>
-        <Link to="/userProfile">User Profile</Link>
-        <Link to="/debits">Debits</Link>
-        <Link to="/credits">Credits</Link>
-
         <AccountBalance accountBalance={this.props.accountBalance} />
+        <div className="links">
+          <NavLink to="/userProfile">User Profile</NavLink>
+          <NavLink to="/debits">Debits</NavLink>
+          <NavLink to="/credits">Credits</NavLink>
+          <NavLink to="/login">Log In</NavLink>
+        </div>
       </div>
     );
   }
